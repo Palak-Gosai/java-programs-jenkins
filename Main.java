1bad6c2656c388a9b4310728687ed37d2a9fd768
Palak-Gosai <palak.gosai263@gmail.com>
2024-08-17 22:10:22 +0530

    initial commit

--- Main.java
+++ Main.java
@@ -5,5 +5,6 @@ class Main{
         System.out.println("Hello Worold");
         System.out.println(" This is java program jenkins testing");
         System.out.println(new Date());
+        System.out.println("after git desktop");
     }
 }
