6948801dc49f1d32292ff773d897fa54116e629f
Palak-Gosai <palak.gosai263@gmail.com>
2024-08-17 22:21:44 +0530

    added one more file

--- Main.java
+++ Main.java
@@ -2,9 +2,10 @@ import java.util.Date;
 
 class Main{
     public static void main(String[] args) {
-        System.out.println("Hello Worold");
+        System.out.println("Hello World");
         System.out.println(" This is java program jenkins testing");
         System.out.println(new Date());
         System.out.println("after git desktop");
+        System.out.println("added one more file");
     }
 }
